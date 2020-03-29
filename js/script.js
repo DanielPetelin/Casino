6ff956cbf8d322821a710e577f172b37b49b2a44
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-03-29 18:36:07 +0300

    add second commit

--- js/script.js
+++ js/script.js
@@ -10,5 +10,21 @@ square.forEach(function(element) {
     let randNum = random(0, img.length - 1);
     console.log(randNum);
     element.style.backgroundImage = 'url(images/' + img[randNum] + '.png';
-    
+    element.dataset.data = randNum;
+    element.onclick = changeImage;
 });
+
+function changeImage() {
+    document.querySelector('#out').innerHTML = '';
+    let data = this.dataset.data;
+    console.log(data);
+
+    if (+data + 1 < img.length) {
+        data++;
+    } else {
+        data = 0;
+    };
+
+    this.dataset.data = data;
+    this.style.backgroundImage = 'url(images/' + img[data] + '.png';
+};
